8fe2c70bc74cdc06d4bad9c690090cf0a8674628
Brad Allred <bradleyjall@yahoo.com>
2015-04-26 19:12:41 -0600

    Countdown introduction

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -18,13 +18,41 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    NSString *myName = @"Brad";
+    
+    countDownTilIntroduction(4);
+    printIntroductions(myName);
+    NSString *introduction;
+    NSString *japaneseIntroduction;
+    
+    introduction = [NSString stringWithFormat:@"My name is %@", myName];
+    japaneseIntroduction = [@"moushimasu" stringByAppendingString: (@"%@", myName)];
+   
+    NSLog(@"%@ %@", introduction, japaneseIntroduction);
     
     
     return YES;
 }
 
-// Define new functions here...
+// Define new functions h
+void printIntroductions(NSString *name){
+    NSString *introduction;
+    NSString *japaneseIntroduction;
+    
+    introduction = [NSString stringWithFormat:@"My name is %@", name];
+    japaneseIntroduction = [@"moushimasu" stringByAppendingString: (@"%@", name)];
+    
+    NSLog(@"%@ \n %@", introduction, japaneseIntroduction);
+}
 
-
+void countDownTilIntroduction(int numberOfDays){
+    if(numberOfDays == 0) {
+        NSLog(@"The time has come");
+    }else{
+        NSLog(@"%d days till introductions", numberOfDays);
+        int oneLessDay = numberOfDays - 1;
+        countDownTilIntroduction(oneLessDay);
+    }
+}
 
 @end
